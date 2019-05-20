05769bf86bb70af5ffd661d8f13257bce6dab0f0
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-20 16:07:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   newDriver[key] = value;
   return newDriver;
 }
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+}
